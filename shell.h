36179604586c54d9ad28ef4418da42322289ce99
Romaric250 <lonfonyuyromeo@gmail.com>
2023-05-17 00:34:36 -0700

    more str functins coded

--- shell.h
+++ shell.h
@@ -9,7 +9,9 @@ char *_strcpy(char *desk, char *src);
 size_t _strncmp(char *str1, char *str2, size_t k);
 char *_strcat(char *desk, char *src);
 int _strcmp(char *str1, char *str2);
-
+size_t _strpn(const char *str1, const char *str2);
+char *_strchr(const char *str, int chars);
+size_t _strspn(const char *str1, const char *str2);
 
 
 
