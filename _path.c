f32c3abbc6297595e8f0f5dc0bcc19d39b4ab87a
Romaric250 <lonfonyuyromeo@gmail.com>
2023-05-17 22:14:29 -0700

    function to get path done

---
+++ _path.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+/**
+ * _path - this functions gets the var from PATH
+ * @env: this is an environmental variable.
+ * Return: the path value to be be tokenised later with input from user
+ */
+
+char *_path(char **env)
+{
+	char *paths = NULL;
+	size_t k = 0;
+	size_t j = 0;
+	size_t i = 5;
+
+	for (k = 0; _strncmp(env[k], "PATH=", 5); k++)
+		;
+	if (env[k] == NULL)
+		return (NULL);
+	for (i = 5; env[k][j]; j++, k++)
+		;
+	path = malloc(sizeof(char) * (i + 1));
+
+	if (path == NULL)
+		return (NULL);
+
+	for (j = 5, i = 0; env[k][j]; j++, i++)
+		path[i] = env[k][j];
+
+
+	path[i] = '\0';
+
+	return (path);
+}
