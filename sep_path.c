34b1f4007d0bf87fc9464f518e772b457c598f5a
Johntista85 <johntista85@gmail.com>
2023-05-18 08:01:32 -0700

    separated path commit

--- sep_path.c
+++ sep_path.c
@@ -35,37 +35,3 @@ char **separatePath(char *path)
 	command[i] = NULL;
 	return (command);
 }
-/**
- * _path - gets the value of the PATH variable.
- * @env: environmental variables.
- * Return: the value of the PATH variable.
- */
-
-char *_path(char **env)
-{
-	char *path = NULL;
-	size_t i = 0;
-
-	while (env[i] != NULL)
-	{
-		if (_strncmp(env[i], "PATH=", 5) == 0)
-		{
-			path = _strdup(env[i] + 5);
-			break;
-		}
-		i++;
-	}
-	return (path);
-}
-
-int main(void)
-{
-	char *path = _path(environ);
-	char **path_components = separatePath(path);
-
-	free(path);
-	free(path_components);
-
-	return (0);
-}
-
